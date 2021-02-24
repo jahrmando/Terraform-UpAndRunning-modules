1e23032768be0c24822777fd717ad4463c40c7ba
Armando Uch <armando.uch@pm.me>
2021-02-24 12:41:32 -0600

    Add other lifecycle to ASG

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -71,6 +71,8 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
+  name = "${var.cluster_name}-${aws_launch_configuration.example.name}"
+
   launch_configuration  = aws_launch_configuration.example.name
   vpc_zone_identifier   = data.aws_subnet_ids.default.ids
 
@@ -79,6 +81,12 @@ resource "aws_autoscaling_group" "example" {
 
   min_size = var.min_size
   max_size = var.max_size
+
+  min_elb_capacity = var.min_size
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tag {
     key                 = "Name"
